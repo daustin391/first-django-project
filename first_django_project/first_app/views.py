99efff05a162b79c1bf4bceea55763f11b87bdc1
Dave Austin <d.austin_391@yahoo.com>
2020-10-11 14:42:44 -0400

    added 'show' method

--- first_django_project/first_app/views.py
+++ first_django_project/first_app/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
+
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number {number}")
